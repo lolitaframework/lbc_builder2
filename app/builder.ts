4462d0229221c97ed8bdac3f88b0355117972fba
Vitaliy Shebela <vitalik.privat@gmail.com>
2016-12-13 18:18:26 +0200

    Refactored

--- app/builder.ts
+++ app/builder.ts
@@ -1,7 +1,7 @@
 // This is start point of application
-require('normalize-css/normalize.css');
-require('font-awesome/css/font-awesome.css');
+
 import * as templates from './core/ts/templates';
+require('normalize-css/normalize.css');
 
 function routing(locals) {
     switch (locals.path) {
--- app/modules/logo/build.ts
+++ app/modules/logo/build.ts
@@ -1,7 +1,9 @@
 import * as templates from '../../core/ts/templates';
+require('font-awesome/css/font-awesome.css');
 
 let template = require('./tpl/default.hbs');
 let styles = require('./scss/logo.scss');
+
 
 let params = {
     name: 'Vitaliy',
